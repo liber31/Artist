5e9451178fd1a1e0e230e9f69b725375c0b5f4a7
rhea-so <jeonghyeon.rhea@gmail.com>
2021-01-10 17:50:09 +0900

    test: sound play는 구글로 인해 막힌듯

--- resources/js/index.js
+++ resources/js/index.js
@@ -15,87 +15,15 @@ setFps(60);
 
 const global = window.variables;
 
-const text_array = ['안녕하세요', 'Hello', 'こんにちは', '你好', 'Xin chào', 'Ciao', 'Guten Tag', 'Hola', 'Hej', 'salve', 'Apa kabar', 'merhaba', 'Habari za kucha', 'Сайн байна уу', 'Здравствуйте', 'mirëmëngjes', 'Bonjour'];
-try {
-    setTimeout(() => {
-        let index = 0;
-        setInterval(() => {
-            const text = instanceCreate(Text, 0, 0, 2);
-            text.text = text_array[index];
-            index++;
-            index %= text_array.length;
-        }, 1000);
-    }, 0);
-
-    setTimeout(() => {
-        setTargetSize(720, 1280);
-    }, 5000);
-
-} catch (err) {
-    alert(err);
-}
-
-class Text extends ArtistElement {
+class Tester extends ArtistElement {
     create() {
-        this.size = irandomRange(1, 5);
-        this.text = '안녕하세요';
-        this.position_x = 1300;
-        this.position_y = irandomRange(0, 1000);
-        this.alpha = 0;
-        this.target_alpha = randomRange(0.2, 0.8);
-    }
-
-    update() {
-        this.x = (global.WIDTH / 1000) * this.position_x;
-        this.y = (global.HEIGHT / 1000) * this.position_y;
-        this.alpha += (this.target_alpha - this.alpha) / 30;
-        this.position_x -= this.alpha * 5;
-
-        if (this.x < global.WIDTH / 2) {
-            instanceDestroy(this);
-        }
-    }
-
-    draw() {
-        drawSetAlpha(this.alpha);
-        drawSetColor('black');
-        drawSetFont(
-            30 * this.size,
-            'Arial');
-        drawTextTransformed(
-            this.x,
-            this.y,
-            this.text,
-            'center',
-            0
-        );
-        drawSetAlpha(1);
+        // new Audio('http://localhost:8080/sound/wiiShop.mp3').play();
     }
 }
 
-class Test extends ArtistElement {
-    create() {
-        setTimeout(() => {
-            instanceDestroy(this);
-        }, 3000);
-    }
-    draw() {
-        drawSetAlpha(this.alpha);
-        drawSetColor('black');
-        drawSetFont(
-            30,
-            'Arial');
-        drawTextTransformed(
-            global.WIDTH / 2,
-            global.HEIGHT / 2,
-            'TEST MESSAGE',
-            'center',
-            0
-        )
-        drawSetAlpha(1);
-    }
-}
+setTimeout(() => {
 
-instanceCreate(Test, 0, 0, 2)
+    instanceCreate(Tester, 0, 0, 2)
+}, 1000);
 
 start();
