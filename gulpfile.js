ffeaa1c2507c633e540682c4362b85005029fe44
rhea-so <jeonghyeon.rhea@gmail.com>
2021-01-13 20:50:41 +0900

    update: web에서 import 가능하도록 수정

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 const gulp = require('gulp'),
-	uglify = require('gulp-uglify-es').default,
-	javascriptObfuscator = require('gulp-javascript-obfuscator');
+	uglify = require('gulp-uglify-es').default;
+// javascriptObfuscator = require('gulp-javascript-obfuscator');
 
 
 gulp.task('uglify', () => {
 	return gulp.src('src/**/artist.js')
 		.pipe(uglify())
-		.pipe(javascriptObfuscator({
-			compact: true,
-			renameGlobals: true,
-			unicodeEscapeSequence: true,
-			splitStrings: true,
-			selfDefending: true,
-			controlFlowFlattening: true,
-		}))
+		// .pipe(javascriptObfuscator({
+		// 	compact: true,
+		// 	renameGlobals: true,
+		// 	unicodeEscapeSequence: true,
+		// 	splitStrings: true,
+		// 	selfDefending: true,
+		// 	controlFlowFlattening: true,
+		// }))
 		.pipe(gulp.dest('./'));
 });
 
