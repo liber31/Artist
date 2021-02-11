9fa816df9f676c7f05a62bf4f62a57d5ab2a3e09
rhea-so <rhea31@naver.com>
2021-02-11 15:26:48 +0900

    update: Idea

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -2,4 +2,13 @@ import { Artist } from './artist.js';
 
 Artist.init('canvas', 720, 1280);
 const test = Artist.createElement();
+//test.routine((pencil) => {
+//        pencil
+//            .drawLine()
+//            .drawLine()
+//            .drawLine()
+//            .drawRectangle()
+//            
+//        alert(pencil);
+//    }); // 이렇게 되면 좋겠다!
 Artist.start();
